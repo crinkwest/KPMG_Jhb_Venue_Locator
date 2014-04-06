837161c7600fefa1bf0c5ca92d031ac4e790ee44
unknown <crinkwest@.(none)>
2014-04-06 18:56:35 +0200

    IT's WORKING Yaaaayyyyyyyy :)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 
-function getBoardroomLocation(aBoardroom)
+function getVenueLocation(aVenue)
 {
-	/* First we need to discover the Building and Floor the Boardroom is on */
-	//var vBoardroomList = document.getElementById("boardroomListID");
-	//var vBoardroomId = vBoardroomList.options[vBoardroomList.selectedIndex].value;
-	var vBoardroomId = aBoardroom.value;
-	
-	var vBuilding;
-	switch (vBoardroomId)
+/*	switch (vBoardroomId)
 	{
 		case "acaciaRoomId":
 		case "baobabRoomId":
@@ -40,17 +34,47 @@ function getBoardroomLocation(aBoardroom)
 			alert(vBuilding);
 			break;
 		default:
+			vBuilding = "Building not found";
 			alert("Hey! I don't know this Boardroom dude: " + vBoardroomId);
 		
+	}*/
+
+	/* First we need to discover the Building and Floor the venue is on */
+	//var vBoardroomList = document.getElementById("boardroomListID");
+	//var vBoardroomId = vBoardroomList.options[vBoardroomList.selectedIndex].value;
+	var vVenueId = aVenue.value;
+	var vBuilding;	
+	var fp = {
+		wallStreetRoomId: function() { vBuilding = "Crescent_1"; },
+		visionRoomId: function () { vBuilding = "Crescent_2"; },
+		unknown: function() { throw "Error! The venue: " + vVenueId + " was not found";}
+	};
+	fp.acaciaRoomId = fp.baobabRoomId = fp.crescent1RoomId = fp.crescent2RoomId = fp.crescent3RoomId = fp.ebonyRoomId = fp.mahoganyRoomId = fp.msasaRoomId = fp.platinumRoomId = fp.sccm1RoomId = fp.sccm2RoomId = fp.sccm3RoomId = fp.tambotiRoomId = fp.tanzaniteRoomId = fp.wallStreetRoomId;
+	fp.achieveRoomId = fp.cres4RoomId = fp.cres5RoomId = fp.energyRoomId = fp.FIOSRoomId = fp.successRoomId = fp.teamworkRoomId = fp.TnLRoomId = fp.visionRoomId;
+	try {
+		fp[ vVenueId ] ? fp[ vVenueId ]() : fp.unknown();
+	} catch (err) {
+		alert(err);
+		return;
 	}
+	alert(vBuilding);
 	
 	// todo: if statement for each building floor
-  	displayCrescentLevel_2(vBuilding, vBoardroomId);
+  	displayFloorPlan(vBuilding, vVenueId);
+  	
+  	/* Now we will update the room details */
+	displayRoomDetails(vVenueId);
+	//document.getElementById("paraDetails").innerHTML = vSelectedValue;
 };
   
-function displayCrescentLevel_2(aBuilding, aVenue)
+function displayFloorPlan(aBuilding, aVenue)
 {
-	var vSVG = document.getElementById("floorPlan");
+	//var vFloorplan = document.getElementById("floorPlan");
+	var vObject = document.getElementsByTagName("OBJECT")[0];	//get the first element of the list of objects
+	alert(vObject.id);
+	var vSVG = document.createElement("OBJECT");
+	
+	vSVG.setAttribute("id", "floorPlan");
 	vSVG.setAttribute("type", "image/svg+xml");
 	  		
 	if (aBuilding == "Crescent 0"){
@@ -62,11 +86,13 @@ function displayCrescentLevel_2(aBuilding, aVenue)
 		alert("Crescent 2 floorplan to be set");
 		vSVG.setAttribute("data", "img/crescent2map.svg"); 		
 	}
-	vSVG.style.visibility = 'visible';
-	//vSVG.style.display = "inline";
-	//document.replaceChild(vSVG);
+	//vSVG.style.visibility = 'visible';	//visible attribute keeps everything in place, so use this rather
+	//vSVG.style.display = "inline";		//display set to none will move all the ojects around it.
+	var vDIV = document.getElementById("container");
+	//vDIV.appendChild(vSVG);
+	vDIV.replaceChild(vSVG, vObject);
 	
-	alert("gettting content");
+	alert("getting contentDocument");
 	var vSvgDoc = vSVG.contentDocument;
 	/* Since we don't know what the previous room selection was, we need to set all rooms to the default */
 /*		vSvgDoc.getElementById("achieveRoomId").getAttributeNode("style").value="fill:#800080;stroke:none;opacity:0.5";
@@ -82,14 +108,13 @@ function displayCrescentLevel_2(aBuilding, aVenue)
 	/* Now we can highlight the selected room */
 	var vSVGRoom = vSvgDoc.getElementById(aVenue);
 	vSVGRoom.getAttributeNode("style").value="fill:#800080;stroke:orange;opacity:1";
-	alert("calling function to show details");
-	/* Now we will update the room details */
-	displayRoomDetails(aVenue);
-	//document.getElementById("paraDetails").innerHTML = vSelectedValue;
+	return;
 };
   
-function displayRoomDetails(aRoomId)
+function displayRoomDetails(aMeetingRoomId)
 {
+	alert("in displayRoomDetails");
+	
 	var vRoomName;
 	var vBuilding;
 	var vFloor;
@@ -102,7 +127,7 @@ function displayRoomDetails(aRoomId)
 	var vVideoConf;
 	var vTeleConf;
 	
-	switch (aRoomId)
+	switch (aMeetingRoomId)
 	{
 		case "achieveRoomId":
 			vRoomName = "Achievement Boardroom";
